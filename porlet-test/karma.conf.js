6a8d131438cf001bcd8d8878566cb5029d4984dd
Juan Belmonte Rodríguez <reonemail@gmail.com>
2018-04-03 15:36:51 +0200

    Use a middleware to handle osgi-dependent templateUrls in Angular components

--- porlet-test/karma.conf.js
+++ porlet-test/karma.conf.js
@@ -5,13 +5,14 @@ var webpackConfig = require('./config/webpack.config.test');
 
 var package = require('./package.json');
 var MODULE_ID = package.name;
-var PORTLET_RELATIVE_URL = new RegExp(`/o/${MODULE_ID}`, 'ig');
+var PORTLET_RELATIVE_URL = new RegExp(`/o/${MODULE_ID}/((.+\.html)|(.+\.json)|(.+\.css))`, 'ig');
 var SOURCE_ROOT = 'src/main/resources/META-INF/resources';
 
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
+    middleware: ['liferay-template-url'],
     plugins: [
       require('karma-webpack'),
       require('karma-jasmine'),
@@ -20,6 +21,20 @@ module.exports = function (config) {
       // require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       // require('karma-coverage-istanbul-reporter'),
+      {
+        'middleware:liferay-template-url': ['factory', function (config) {
+          return function (req, res, next) {
+            if (PORTLET_RELATIVE_URL.test(req.url)) {
+              var url = req.url.replace(PORTLET_RELATIVE_URL, '/base/' + SOURCE_ROOT + '/$1');
+              // console.log('Rewrite URL ' + req.url + ' to ' + url);
+              res.writeHead(301, {Location: url});
+              res.end();
+              return;
+            }
+            next();
+          };
+        }]
+      }
     ],
     preprocessors: {
       'src/**/*.ts': ['webpack', 'sourcemap']
@@ -39,7 +54,7 @@ module.exports = function (config) {
       'node_modules/zone.js/dist/jasmine-patch.js', // 'TypeError: Cannot read property 'assertPresent' of undefined'
       'node_modules/zone.js/dist/async-test.js', // 'TypeError: Cannot read property 'assertPresent' of undefined'
       'node_modules/zone.js/dist/fake-async-test.js', // 'TypeError: Cannot read property 'assertPresent' of undefined'
-      SOURCE_ROOT + '/**/*.html',
+      SOURCE_ROOT + '/**/*.(html|css|json)',
       SOURCE_ROOT + '/**/*.spec.ts'
     ],
     // exclude: [
